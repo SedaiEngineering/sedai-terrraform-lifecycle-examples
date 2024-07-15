02b73bf23cf7f01371dc015719445747546226a4
Arun Ramesh <arun@sedai.io>
2024-07-15 22:31:56 +0530

    ECS autoscaling changes

---
+++ iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "ecs_autoscaling_role" {
+  name = "ecsAutoscalingRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "application-autoscaling.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+  ]
+}
